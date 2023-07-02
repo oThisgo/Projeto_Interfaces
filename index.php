429cb6444d55351f7bbc06b90cdc7c9a54115242
Thiago Schiedeck <thiagosds1701@gmail.com>
2023-07-02 16:41:46 -0300

    02/07 (16:41)

--- index.php
+++ index.php
@@ -70,16 +70,46 @@ include_once 'Includes/header.php';
                         while($row1 = mysqli_fetch_array($resultado1)){
                             echo '
                             <div id="produtos">
-                                <img id="img-produtos" src="Images/' . $row1['imagem'] . '">
+                                <img height=60% width=60% style="display: block; margin-right: auto; margin-left: auto;" id="img-produtos" src="Images/' . $row1['imagem'] . '">
                                 <h3 id="nome-produtos">' . $row1['nome'] . '</h3>
                                 <h3 id="preco-produtos">R$' . $row1['preco'] . '</h3>
-                                <a id="comprar-home" href="produto.php">COMPRAR</h3>
+                                <a id="comprar-home" href="produto.php">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-cart-fill" viewBox="0 0 16 16">
+                                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                    </svg>&nbspCOMPRAR
+                                </a>
                             </div>';
                         }
         
                     }
                 }
             }
+        ?>
+
+        <h3 class="secao" id="todos">TODOS</h3>
+
+        <?php
+
+            $sql2 = "SELECT * FROM produtos";
+            $resultado2 = mysqli_query($db,$sql2);
+
+            if($resultado2){
+
+                while ($row2 = mysqli_fetch_array($resultado2)){
+                    echo '
+                    <div id="produtos">
+                        <img height=60% width=60% style="display: block; margin-right: auto; margin-left: auto;" id="img-produtos" src="Images/' . $row2['imagem'] . '">
+                        <h3 id="nome-produtos">' . $row2['nome'] . '</h3>
+                        <h3 id="preco-produtos">R$' . $row2['preco'] . '</h3>
+                        <a id="comprar-home" href="produto.php">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-cart-fill" viewBox="0 0 16 16">
+                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </svg>&nbspCOMPRAR
+                        </a>
+                    </div>';
+                }
+            }
+
         ?>
 
     </div>
